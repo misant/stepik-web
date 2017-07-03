c1467d796d8fe3108037fe124a3974334d22aa09
root <root@localhost.localdomain>
2017-07-04 00:37:35 +0300

    add nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/misant/stepic_web.git /home/box/web
+sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test

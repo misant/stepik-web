7f7455b6e5bf2f601d9f122f0d165a42842654d6
misant <jbe@mail.ru>
2017-07-06 20:59:42 +0000

    hello.py tested

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/helo.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
